7f2da295b06a61f6d883b9417c3b01f6510690a1
William M <cominatyou@cominatyou.com>
2023-07-24 17:48:37 -0500

    Fix padding when removing views button

--- views-button-remover/main.js
+++ views-button-remover/main.js
@@ -1,6 +1,8 @@
 new MutationObserver((_mutationList, _observer) => {
     document.querySelectorAll("[href$=analytics]").forEach(e => {
         if (e.getAttribute("data-testid") !== "analyticsButton") { // preserve the 'view tweet analytics' button
+            // add extra padding on the right
+            e.parentElement.parentElement.appendChild(document.createElement("div"));
             e.parentElement.remove();
         }
     });
