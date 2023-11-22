8c1ac68a9afd1d65f27adbd646530801f9412b49
William M <cominatyou@cominatyou.com>
2023-11-21 21:37:00 -0600

    Update CSS selectors and conditions for sidebar and tweet text styles

--- branding-restorer/icon.js
+++ branding-restorer/icon.js
@@ -1,4 +1,4 @@
-const TOP_ICON_SELECTOR = "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > header > div > div > div > div.css-1dbjc4n.r-1habvwh > div.css-1dbjc4n.r-dnmrzs.r-1vvnge1 > h1 > a > div > svg > g > path";
+const TOP_ICON_SELECTOR = "#react-root > div > div > div.css-175oi2r.r-13qz1uu.r-417010.r-18u37iz > header > div > div > div > div.css-175oi2r.r-1habvwh > div.css-175oi2r.r-dnmrzs.r-1vvnge1 > h1 > a > div > svg > g > path";
 
 new MutationObserver(async () => {
     if (document.querySelector(TOP_ICON_SELECTOR)) {
--- tweet-text-enlarger/tweet-text.js
+++ tweet-text-enlarger/tweet-text.js
@@ -1,9 +1,12 @@
-const LIGHT_CLASS_LIST = "div.css-901oao.r-18jsvk2.r-37j5jr.r-1inkyih.r-16dba41.r-135wba7.r-bcqeeo.r-bnwqim.r-qvutc0";
-const DIM_CLASS_LIST = "div.css-901oao.r-vlxjld.r-37j5jr.r-1inkyih.r-16dba41.r-135wba7.r-bcqeeo.r-bnwqim.r-qvutc0";
-const LIGHTS_OUT_CLASS_LIST = "div.css-901oao.r-1nao33i.r-37j5jr.r-1inkyih.r-16dba41.r-135wba7.r-bcqeeo.r-bnwqim.r-qvutc0";
+const LIGHT_CLASS_LIST = "div.css-1rynq56.r-bcqeeo.r-qvutc0.r-37j5jr.r-1inkyih.r-16dba41.r-bnwqim.r-135wba7";
+const DIM_CLASS_LIST = "div.css-1rynq56.r-bcqeeo.r-qvutc0.r-37j5jr.r-1inkyih.r-16dba41.r-bnwqim.r-135wba7";
+const LIGHTS_OUT_CLASS_LIST = "div.css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6,r-rjixqe.r-16dba41.r-bnwqim";
 
 new MutationObserver((_mutationList, _observer) => {
+    if (!window.location.href.includes("/status/")) return; // do not run when a timeline is being viewed
     document.querySelectorAll(`${LIGHT_CLASS_LIST}, ${DIM_CLASS_LIST}, ${LIGHTS_OUT_CLASS_LIST}`).forEach(e => {
+        // do not run for comments in a thread, or children of quote tweets
+        if ((e.parentElement.classList.length === 1 && e.parentElement.classList.contains("css-175oi2r")) || e.closest("div.css-175oi2r.r-adacv.r-1udh08x.r-1kqtdi0.r-1867qdf.r-rs99b7.r-o7ynqc.r-6416eg.r-1ny4l3l.r-1loqt21")) return;
         e.classList.remove(".r-1inkyih");
         e.querySelector("span").style.lineHeight = "28px";
         e.style.fontSize = "23px";
